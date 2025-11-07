af42aaafb88c7170d967ad310050de0df29054f7
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-11-07 08:40:52 +0700

    fix: fix conflict

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-<<<<<<< HEAD
-      { protocol: "https", hostname: "sununi.edu.vn" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" },
-=======
       {
         protocol: "https",
         hostname: "sununi.edu.vn",
@@ -22,7 +17,6 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
->>>>>>> 9769a0b46c38a65df8a33d8e386b48456435259e
     ],
     unoptimized: true, 
     dangerouslyAllowSVG: true,
